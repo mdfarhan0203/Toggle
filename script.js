9fcada791dd2db95b762403b60025ce6dae9863c
MD FARHAN <50393822+mdfarhan0203@users.noreply.github.com>
2023-02-21 00:06:40 +0530

    initially commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".container").click(function() {
+    $('body').toggleClass("body-color");
+    $(".container").toggleClass("button-container");
+    $("h1").toggleClass("text");
+    $(".button").toggleClass("button-1");
+});  
+
+
+
+
+
